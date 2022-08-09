e70dd63633ea979f96e88da186085f6f58af850d
laipuran <55612059+laipuran@users.noreply.github.com>
2022-08-09 15:31:48 +0800

    Fixed a bug

--- OverTop/Floatings/AppWindow.xaml.cs
+++ OverTop/Floatings/AppWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 using static OverTop.AppWindowClass;
 
 namespace OverTop.Floatings
