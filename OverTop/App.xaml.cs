736d708a903852a1522f6bc0e30cce07471f17c7
Puran Lai <1696095803@qq.com>
2023-04-05 16:58:21 +0800

    Change: Code Cleared

--- OverTop/App.xaml.cs
+++ OverTop/App.xaml.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows;
 using static OverTop.Settings;
 
--- OverTop/FunctionalWindows/SplashWindow.xaml.cs
+++ OverTop/FunctionalWindows/SplashWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace OverTop.FunctionalWindows
 {
@@ -35,7 +24,8 @@ namespace OverTop.FunctionalWindows
 
         private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
         {
-            this.Dispatcher.Invoke(() => {
+            this.Dispatcher.Invoke(() =>
+            {
                 this.Close();
             });
         }
